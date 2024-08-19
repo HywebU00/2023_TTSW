63a109aed3fd92560f18e27035e32723f50d6196
Tseng <tseng7510@gmail.com>
2024-08-19 14:31:31 +0800

    更新

--- B/js/customize.js
+++ B/js/customize.js
@@ -2402,41 +2402,6 @@ if ($('.newsBox .slider').length > 0) {
     // centerMode: true,
     // appendArrows: $(`.newsBox .tabBar`),
   });
-
-  $('.newsBox .slider ul').slick({
-    lazyLoad: 'ondemand',
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    arrow: true,
-    infinite: false,
-    slidesToScroll: 1,
-    prevArrow: '<button class="slick-prev" id="test" aria-label="Previous" type="button" title="Previous"><svg><use xlink:href="#arrow3" /></svg></button>',
-    nextArrow: '<button class="slick-next" aria-label="Next" type="button" title="Next"><svg><use xlink:href="#arrow3" /></svg></button>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
 }
 
 if ($('.lp .newTopNav').length > 0) {
